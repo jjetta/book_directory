47e4375106eeeae03a7da3c50d128f7311827fae
jjetta <johnjoshetta@gmail.com>
2026-07-02 11:02:24 -0500

    finished BookService

--- src/main/java/com/jjetta/book_directory/service/BookService.java
+++ src/main/java/com/jjetta/book_directory/service/BookService.java
@@ -5,6 +5,7 @@ import com.jjetta.book_directory.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -15,15 +16,37 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
-    public Book createBook(Book book) {}
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
 
-    public List<Book> getAllBooks() {}
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 
-    public Book getBookById(Long id) {}
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
+    }
 
-    public Book updateBook(Long id, Book bookDetails) {}
+    public Book updateBook(Long id, Book bookDetails) {
+        Book existingBook = bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
 
-    public void deleteBook(Long id) {}
+        existingBook.setTitle(bookDetails.getTitle());
+        existingBook.setAuthor(bookDetails.getAuthor());
+        existingBook.setYearPublished(bookDetails.getYearPublished());
+        existingBook.setIsbn(bookDetails.getIsbn());
+
+        return bookRepository.save(existingBook);
+    }
+
+    public void deleteBook(Long id) {
+        if (!bookRepository.existsById(id)) {
+            throw new RuntimeException("Book not found with id: " + id);
+        }
+        bookRepository.deleteById(id);
+    }
 
 
 
